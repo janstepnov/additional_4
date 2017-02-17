f350d940e15d677ebb7cd808ee58e108c7abac43
janstepnov <janstepnov@gmail.com>
2017-02-17 20:49:57 +0300

    task solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 module.exports = function multiply(first, second) {
-  // your solution
+
+    var numm1 = first.split('').reverse();
+    var numm2 = second.split('').reverse();
+    var result = [];
+
+    for (var i=0; numm1[i] >=0; i++){
+        for(var j=0; numm2[j] >=0; j++){
+            if (!result[i+j]) result [i+j] = 0;
+            result[i+j] += (numm1[i]* numm2[j]);
+        }
+    }
+    for (var k=0; result[k] >=0; k++){
+        if (result[k] >=10){
+            if(!result[k+1]) result[k+1] = 0;
+            result[k+1] += parseInt(result[k] / 10);
+            result[k] = result[k] % 10;
+        }
+    }
+    var sol = result.reverse().join('');
+    return sol;
 }
